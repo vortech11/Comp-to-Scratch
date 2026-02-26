e2aa55bfd9fc0c2d238cbc0f73804e3ac4e3c687
vortech11 <doc.m.brendan@gmail.com>
2026-02-25 22:22:54 -0600

    Cleaned up unused code from fileReader.py

--- src/parser/fileReader.py
+++ src/parser/fileReader.py
@@ -1,8 +1,4 @@
-from pathlib import Path
-
 def loadFile(fileName) -> str:
-    filePath: Path = Path(fileName).resolve()
-    print(filePath)
     with open(fileName, "r") as file:
         fileData = ""
         for line in file:
